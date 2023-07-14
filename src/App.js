90e5fa44a4c89169be2b7901b8162043c8724bb3
Supreeti Kushwaha <sksupreetk11@gmail.com>
2023-07-15 01:06:04 +0530

    resolve errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Navigation from './components/navigation';
-import Main from './components/main';
-import Category from './components/categories';
+import Main from './Components/main';
+import Navigation from './Components/navigation';
+import Category from './Components/categories';
 
 function App() {
   return (
